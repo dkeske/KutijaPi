89a61744e19831e5484cfe76f2f2f1bc4b637e2d
Daniel Kostic <danielkeske@hotmail.com>
2015-11-23 17:36:57 +0100

    Tabs to spaces

--- main/main.py
+++ main/main.py
@@ -5,22 +5,22 @@ from postServer import postServer,saveToFile
 from threading import Thread 
 
 def countCaps():
-	GPIO.setmode(GPIO.BCM)
+    GPIO.setmode(GPIO.BCM)
 
-	ECHO = 23
-	GPIO.setup(ECHO, GPIO.IN)
-	broj = 0
-	prethodni = 0
-	while True:
-	    tekuci = GPIO.input(ECHO)
-	    
-	    if (tekuci == 0 and prethodni == 1):
-	        broj = broj + 1
-	        print(broj)
-	        if(broj==3):
-			saveToFile(broj)
-		       	broj = 0
-	           
-	    prethodni = tekuci
+    ECHO = 23
+    GPIO.setup(ECHO, GPIO.IN)
+    broj = 0
+    prethodni = 0
+    while True:
+        tekuci = GPIO.input(ECHO)
+        
+        if (tekuci == 0 and prethodni == 1):
+            broj = broj + 1
+            print(broj)
+            if(broj==3):
+            saveToFile(broj)
+                broj = 0
+               
+        prethodni = tekuci
 t1 = threading.Thread(target=countCaps, args=[])
 t1.start()
--- main/postServer.py
+++ main/postServer.py
@@ -2,15 +2,15 @@ __author__ = 'Keske'
 import requests
 
 def postServer(number, isFull, boxID):
-	payload = {'NumberOfCaps':number, 'isFull':isFull, 'Box':boxID}
-	r = requests.post("http://192.168.0.101:8000/api/", data=payload)
+    payload = {'NumberOfCaps':number, 'isFull':isFull, 'Box':boxID}
+    r = requests.post("http://192.168.0.101:8000/api/", data=payload)
 
 def saveToFile(number):
-	f = open('capLogs.txt', 'r+')
-	old = f.read().strip()
-	if not old.isdigit():
-		old = 0
-	new = int(old)+number
-	f.seek(0)
-	f.write(str(new))
-	f.close()
+    f = open('capLogs.txt', 'r+')
+    old = f.read().strip()
+    if not old.isdigit():
+        old = 0
+    new = int(old)+number
+    f.seek(0)
+    f.write(str(new))
+    f.close()
